5d3dcfd67e5e33cb9669af691d830ea45cd17168
Chris Knox <cjknox1@gmail.com>
2017-03-30 17:40:07 -0400

    defined variable allocation

--- importer.py
+++ importer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
-"""" Importer.py, by Chris Knox 2017_03_28
+""" Importer.py, by Chris Knox 2017_03_28
 This program will find the image directory, change the name, and save to another directory"""
+"""Variable allocation
+    ans= answer to MRN query
+    mrn_count= MEEI MRN number
+    mrn= length of the ans"""
 
 print("IO Image Importer")
 
